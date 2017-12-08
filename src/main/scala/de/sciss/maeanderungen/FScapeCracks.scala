3d6e908e51191ae05c32042f530a34aca34dbf8f
Hanns Holger Rutz <contact@sciss.de>
2017-12-08 23:10:00 +0100

    ok, now we can experiment

--- src/main/scala/de/sciss/maeanderungen/FScapeCracks.scala
+++ src/main/scala/de/sciss/maeanderungen/FScapeCracks.scala
@@ -16,6 +16,7 @@ object FScapeCracks {
     // coordinates in fPolesIn are already up-sampled to original image dimension
     val fPolesIn          = file("/data/projects/Maeanderungen/cracks/cracks2_poles.aif")
     val fImgIn            = file("/data/projects/Imperfect/cracks/two_gray/cracks2_19.jpg")
+//    val fImgIn            = file("/data/projects/Imperfect/cracks/two_bw/cracks2_19bw.png")
     val fAudioOut         = file("/data/temp/test.aif")
     val imgWidth          = 5184
     val imgHeight         = 3020
@@ -27,7 +28,8 @@ object FScapeCracks {
     val g = Graph {
       import graph._
       val poles     = AudioFileIn(fPolesIn, numChannels = 4)
-      val imgIn     = ImageFileIn(fImgIn  , numChannels = 1)
+      val numCh     = 2 // 1
+      val imgIn     = ImageFileIn(fImgIn  , numChannels = numCh)
       val x1        = poles \ 0
       val y1        = poles \ 1
       val x2        = poles \ 2
@@ -36,22 +38,30 @@ object FScapeCracks {
       val dy        = y2 - y1
       val len       = (dx.squared + dy.squared).sqrt
       val pixelStep = 1.0 / audioBreadthStep
-      val numSteps  = (len / pixelStep).floor + (1: GE)
+      val numSteps1 = (len / pixelStep).floor
+      val numSteps  = numSteps1 + (1: GE)
 //      RepeatWindow(Frames(numSteps)).poll(2, "num-steps")
 
       val BUF = 44100
       val numStepsM = BufferMemory(numSteps, BUF)
-      val x         = RepeatWindow(x1.elastic(2), num = len) // XXX TODO
-      val y         = RepeatWindow(y1.elastic(2), num = len) // XXX TODO
+
+      val x         = DEnvGen(levels = x1 zip x2, lengths = numSteps1 zip DC(1))
+      val y         = DEnvGen(levels = y1 zip y2, lengths = numSteps1 zip DC(1))
+//      Plot1D(x, size = 4000, "x")
+//      Plot1D(y, size = 4000, "y")
+
+//      val x         = RepeatWindow(x1.elastic(2), num = len) // XXX TODO
+//      val y         = RepeatWindow(y1.elastic(2), num = len) // XXX TODO
       val scan0     = ScanImage(imgIn, width = imgWidth, height = imgHeight, x = x, y = y, zeroCrossings = 0)
       val scan      = -scan0 + (1.0: GE)
-      val step      = numStepsM.sqrt  // XXX TODO --- let's see if this makes sense
+      val step0     = numStepsM.sqrt
+      val step      = (step0 + WhiteNoise(4)).max(1)
       val lap       = OverlapAdd(scan, size = numStepsM, step = step)
-      val hpf       = OnePole(lap, -0.995)    // XXX TODO --- we need a TwoPole and LeakDC
+      val hpf       = Biquad(lap, b0 = 1, b1 = -1, a1 = -0.99) // dc-block: y(n) = x(n) - x(n-1) + 0.99 * y(n-1)
       val max       = RunningMax(hpf).last
       val disk      = BufferDisk(hpf)
       val sigOut    = disk / max
-      val framesOut = AudioFileOut(fAudioOut, AudioFileSpec(numChannels = 1, sampleRate = 44100), in = sigOut)
+      val framesOut = AudioFileOut(fAudioOut, AudioFileSpec(numChannels = numCh, sampleRate = 44100), in = sigOut)
       ((framesOut - 1) / 44100).poll(44100, "out [s]")
 //      x.poll(1000, "x")
 //      y.poll(1000, "y")
